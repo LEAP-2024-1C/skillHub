d6939f27f6f05dac5edc730759b5d288b74a4c2f
AmaraaTs <amarjargal.ts01@gmail.com>
2024-10-24 14:12:19 +0800

    header footer font

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,18 +1,25 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 import "./globals.css";
 import { Header } from "../components/layout/header";
 import { Footer } from "../components/layout/footer";
+import { Geologica } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+
+const geologica = Geologica({
+  subsets: ["cyrillic"],
+  display: "swap",
+  weight: "400",
 });
 
 export const metadata: Metadata = {
@@ -26,10 +33,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className={geologica.className}>
+      <body className={`antialiased`}>
         <Header />
         {children}
         <Footer />
