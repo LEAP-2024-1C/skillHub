12ed08bbd9661cbef361c6b005a4d7031ce50984
hishgee <nasaa9496@gmail.com>
2024-10-29 11:58:50 +0800

    profile zaswar

--- frontend/src/app/employeerProfile/page.tsx
+++ frontend/src/app/employeerProfile/page.tsx
@@ -1,12 +1,3 @@
-"use client"
-// import {History, Skills} from "@/components/user"
-import { MdOutlineStarRate } from "react-icons/md";
-import { FaCrown } from "react-icons/fa";
-import { IoMdAdd } from "react-icons/io";
-import { IoCameraReverse } from "react-icons/io5";
-import { MdModeEdit } from "react-icons/md";
-import { FaStar } from "react-icons/fa";
-
 
 const Employeer = () => {
 
@@ -14,79 +5,90 @@ const Employeer = () => {
 
     <div className="max-w-[1280px] m-auto min-h-[calc(100vh-326px)] bg-[#f9f9f9]">
 
-       <div className=" h-[400px] flex">
-  
-          <div className="flex flex-col justify-center w-[30%] ">
+       <div className=" h-[360px] flex  ">
+       <div className="pt-[40px] w-[30%] flex flex-col items-center gap-6">
     
-             <div className="avatar flex flex-col items-center gap-5 ">
-                <div className="ring-primary ring-offset-base-100 w-[150px] rounded-full ring ring-offset-2">
-                 <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                </div>
-             </div>
-             <div className="flex mt-[50px] justify-center text-2xl">
-               <FaStar /> 
-               <FaStar />
-               <FaStar />
-               <FaStar />
-               <FaStar />
-             </div>
-  
-          </div>
+    <div className="avatar flex flex-col items-center gap-5 ">
+       <div className="ring-primary ring-offset-base-100 w-[150px] rounded-full ring ring-offset-2">
+        <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+       </div>
+    </div>
+    <input
+       type="file"
+       className="file-input file-input-bordered file-input-success text-xs" />
+   
 
-          <div className="w-[40%] flex flex-col items-center gap-6">
-          <div className=" w-full h-full ">
-              <table className="flex justify-around mt-[70px]">
-                   <thead className="flex flex-col justify-start items-start gap-7 text-xl font-black font-bold text-[#181818]">
-                      <th>Fullname or Company</th>
-                      <th> Type</th>
-                      <th>Email</th>
-                      <th>Phonenumber</th>
-                      <th>Company</th>
+ </div>
 
-                   </thead>
-                   <tbody className="flex flex-col items-end gap-8">
-                       <td>Bat</td>
-                       <td>Dorj</td>
-                       <td>Droj234@gmail.com</td>
-                       <td>+97690302490</td>
-                       <td>Cdf</td>
+          <div className="w-[50%] flex flex-col items-center gap-6">
+          <div className=" w-full flex flex-col h-[360px]">
+              <div className="flex justify-around mt-[30px] ">
+                   <div className="flex flex-col justify-start items-start gap-5 text-xl font-black font-bold text-[#181818]">
+                      <p className="hover:text-[#118a00]">Нэр эсвэл Компани</p>
+                      <p className="hover:text-[#118a00]">Төрөл</p>
+                      <p className="hover:text-[#118a00]">И-Мэйл</p>
+                      <p className="hover:text-[#118a00]">Утасны дугаар</p>
+                      <p className="hover:text-[#118a00]">Компани</p>
+                      <p className="hover:text-[#118a0s0]">Гишүүнчлэл</p>
+
+
+                   </div>
+                   <div className="flex flex-col items-end gap-6">
+                       <p>Бат</p>
+                       <p>Хувь хүн</p>
+                       <p>Bat234@gmail.com</p>
+                       <p>+97690302490</p>
+                       <p>IHC</p>
+                       <p>VIP</p>
                        
-                   </tbody>
-              </table>
-              <textarea name="" id=""></textarea>
+                   </div>
+              </div>
+             <p className="font-black font-bold text-2xl text-[#333333] ml-[75px] mt-[20px] ">Description</p>
           </div>
              
 
           </div>
 
-          <div className="w-[30%] flex flex-col items-end pt-[40px] pr-5 text-2xl text-[#118a00]" >
+          <div className="w-[20%] flex flex-col items-end pt-[30px] pr-10 text-2xl text-[#118a00]" >
             <div className="">
-              <MdModeEdit className="text-2xl"/>
+            <button className="btn w-[170px] hover:bg-[#118a00] hover:text-[#333333] text-xl">Edit</button>
             </div>
-          
-          
-         
           </div>
-        
-          
+
        </div>
+       <div role="tablist" className="tabs tabs-bordered">
+           <input type="radio" name="my_tabs_2" role="tab" className="tab mx-[550px] text-2xl h-[50px] rounded-2xl border-2 bg-base-100 text-[#f9f9f9] hover:text-[#f9f9f9] hover:bg-[#118a00] mt-[40px]" aria-label="Зарын түүх" />
+            <div role="tabpanel" className="tab-disabled bg-base-100 border-base-300 p-6 bg-[] h-[60vh]">
+      
+           <div className=" flex flex-col px-[200px] border-white rounded-lg border-2 w-[80%] p-[20px]  
+               m-auto drop-shadow-lg hover:border-2 hover:border-[#118a00] hover:rounded-xl bg-[#18188] mt-[40px] hover:cursor-pointer">
+         
+               <h1 className="text-[20px] text-[#118a00]"><strong> Тооцооны нягтлан бодогч</strong></h1>
+               <div className="flex justify-between">
+                 <p className="text-[#118a00]">Date:</p>
+                  <p className="text-[#f9f9f9] ml-[145px]">3өдрийн өмнө</p>
+               </div>
+              
+                <div className="flex justify-between gap-10">
+                <h1 className=" text-[#118a00] ">Нэг цагийн үнэлгээ:</h1>
+                   <p className="text-[#f9f9f9]"><strong>20 000₮</strong></p>
+               </div>
+       
+                <div className="flex justify-between gap-5 ">            
+                    <h1 className="text-[#118a00]"><strong>Тавигдах шаардлага:</strong></h1>
+               <p className="text-[#f9f9f9]"><strong>Бакалавр</strong></p>
+              </div>
 
+           </div>
 
-       <div>
-       <div role="tablist" className="tabs tabs-lifted">
-        <input type="radio" name="my_tabs_2" role="tab" className="tab text-2xl pr-[300px]" aria-label="Tab1" />
-         <div role="tabpanel" className="tab-active bg-base-100 border-base-300 rounded-box   ">
-    asdfsafs
-         </div>
-        </div>
-       </div>
+ 
+  </div>
 
-    </div>
-
-
-
-
+</div>
+ 
+</div>
    )
 
 }
 export default Employeer;
+
--- frontend/src/app/userProfile/page.tsx
+++ frontend/src/app/userProfile/page.tsx
@@ -14,56 +14,50 @@ const Freelancer = () => {
 
     <div className="max-w-[1280px] m-auto min-h-[calc(100vh-326px)] bg-[#f9f9f9]">
 
-       <div className=" h-[400px] flex">
+       <div className=" h-[360px] flex pt-[25px] ">
   
-          <div className="flex flex-col justify-center w-[30%] ">
+          <div className="pt-[40px] w-[30%] flex flex-col items-center gap-6">
     
              <div className="avatar flex flex-col items-center gap-5 ">
                 <div className="ring-primary ring-offset-base-100 w-[150px] rounded-full ring ring-offset-2">
                  <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
                 </div>
              </div>
-             <div className="flex mt-[50px] justify-center text-2xl">
-               <FaStar /> 
-               <FaStar />
-               <FaStar />
-               <FaStar />
-               <FaStar />
-             </div>
+             <input
+                type="file"
+                className="file-input file-input-bordered file-input-success bg-red-300 text-xs" />
+            
   
           </div>
 
-          <div className="w-[40%] flex flex-col items-center gap-6">
+          <div className="w-[50%] flex flex-col items-center gap-6 ">
           <div className=" w-full h-full ">
-              <table className="flex justify-around mt-[70px]">
-                   <thead className="flex flex-col justify-start items-start gap-7 text-xl font-black font-bold text-[#181818]">
-                      <th>Firstname</th>
-                      <th>Lastname</th>
-                      <th>Email</th>
-                      <th>Phonenumber</th>
-                      <th>Company</th>
-                   </thead>
-                   <tbody className="flex flex-col items-end gap-8">
-                       <td>Bat</td>
-                       <td>Dorj</td>
-                       <td>Droj234@gmail.com</td>
-                       <td>+97690302490</td>
-                       <td>Cdf</td>
+              <div className="flex justify-around mt-[30px]  ">
+                   <div className="flex flex-col justify-start items-start gap-7 text-2xl font-black font-bold text-[#181818] ">
+                      <p className="hover:text-[#118a00]">Овог нэр</p>
+                      <p className="hover:text-[#118a00]">Өөрийн </p>
+                      <p  className="hover:text-[#118a00]">И-мэйл</p>
+                      <p  className="hover:text-[#118a00]">Утасны дугаар</p>
+                      <p  className="hover:text-[#118a00]">Компани</p>
+                   </div>
+                   <div className="flex flex-col items-end gap-8">
+                       <p>Бат</p>
+                       <p>Дорж</p>
+                       <p>Dorj234@gmail.com</p>
+                       <p>+97690302490</p>
+                       <p>IHC</p>
                        
-                   </tbody>
-              </table>
+                   </div>
+              </div>
           </div>
              
 
           </div>
 
-          <div className="w-[30%] flex flex-col items-end pt-[70px] pr-5 text-2xl text-[#118a00]" >
+          <div className="w-[20%] flex flex-col items-end pt-[30px] pr-10 text-2xl text-[#118a00]" >
             <div className="">
-              <MdModeEdit className="text-2xl"/>
+            <button className="btn w-[170px] hover:bg-[#118a00] hover:text-[#333333] text-xl">Edit</button>
             </div>
-          
-          
-         
           </div>
         
           
@@ -71,9 +65,9 @@ const Freelancer = () => {
 
 
        <div>
-       <div role="tablist" className="tabs tabs-lifted">
-        <input type="radio" name="my_tabs_2" role="tab" className="tab mr-[500px] text-2xl" aria-label="Skills" />
-         <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6 ">
+       <div role="tablist" className="tabs tabs-lifted ">
+        <input type="radio" name="my_tabs_2" role="tab" className="tab mr-[500px] text-2xl h-[50px] text-[#118a00] hover:text-[#f9f9f9] hover:bg-[#118a00] mt-[40px]" aria-label="Ур чадвар" />
+         <div role="tabpanel" className="tab-content bg-base-100 border-base-300  p-6 h-[60vh] ">
           <Skills/>
          </div>
 
@@ -81,10 +75,10 @@ const Freelancer = () => {
         type="radio"
         name="my_tabs_2"
         role="tab"
-        className="tab text-2xl text-blue"
-        aria-label="History"
+        className="tab text-2xl h-[50px] text-[#118a00] hover:text-[#f9f9f9] hover:bg-[#118a00]  "
+        aria-label="Түүх"
         defaultChecked />
-        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
+        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 p-6 h-[60vh]">
          <History/>
         </div>
 
